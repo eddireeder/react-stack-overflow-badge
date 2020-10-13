49beddc6299c81d78e1fe535edc42b7a6f037f0d
Eddie Reeder <e.reeder1997@gmail.com>
2020-10-13 07:46:56 +0100

    Started moving project to library

---
+++ dist/index.d.ts
@@ -0,0 +1,9 @@
+import * as React from "react";
+import "./styles.scss";
+export declare type StackOverflowBadgeProps = {
+    id?: number;
+    card?: boolean;
+    logo?: boolean;
+};
+declare const StackOverflowBadge: React.FC<StackOverflowBadgeProps>;
+export default StackOverflowBadge;
