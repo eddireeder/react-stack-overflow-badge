a3a556c738c1df36cab20e1da33f118288b899d7
Eddie Reeder <e.reeder1997@gmail.com>
2020-10-14 09:31:03 +0100

    Update component width and examples

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,7 +4,14 @@ import { StackOverflowBadge } from 'react-stack-overflow-badge'
 import 'react-stack-overflow-badge/dist/index.css'
 
 const App = () => {
-  return <StackOverflowBadge />
+  return (
+    <div className='App'>
+      <StackOverflowBadge id={2937831} />
+      <StackOverflowBadge id={2937831} card={false} />
+      <StackOverflowBadge id={2937831} logo={false} />
+      <StackOverflowBadge id={2937831} card={false} logo={false} />
+    </div>
+  )
 }
 
 export default App
